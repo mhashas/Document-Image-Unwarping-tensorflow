fixed incorrect calculation in lr scheduler, fixed refine network validation error, summary folder now shorter

core/trainers/trainer.py
@@ -69,6 +69,7 @@ class Trainer(object):
           split (string): current dataset split
         """
         total_loss = 0.0
+        total_ssim = 0.0
         loader = self.train_loader if split == TRAIN else self.test_loader
         bar = tqdm(loader, total=loader.length)
 
@@ -93,19 +94,27 @@ class Trainer(object):
                 gradients = tape.gradient(loss, self.model.trainable_variables)
                 self.apply_gradients(gradients, self.model.trainable_variables, global_step=self.global_step)
             else:
-                output = self.model(image)
+                if self.args.refine_network:
+                    output, second_output = self.model(image)
+                else:
+                    output = self.model(image)
                 loss = self.criterion(target, output)
+
+                if self.args.refine_network:
+                    second_loss = self.criterion(target, second_output)
+                    loss = tf.add(loss, second_loss)
 
                 l2_reg = tf.add_n([tf.nn.l2_loss(v) for v in self.model.trainable_variables if 'bias' not in v.name]) * self.args.weight_decay
                 loss = tf.add(loss, l2_reg)
 
             # Show 10 * 3 inference results each epoch
             if split != VISUALIZATION and i % (loader.length // 10) == 0:
-                self.summary.visualize_image(image, target, output, split=split)
+                image, target, output = self.summary.visualize_image(image, target, output, split=split)
             elif split == VISUALIZATION:
-                self.summary.visualize_image(image, target, output, split=split)
+                image, target, output = self.summary.visualize_image(image, target, output, split=split)
 
             total_loss += loss.numpy()
+            total_ssim += tf.image.ssim(output, target, max_val=1)
             bar.set_description(split + ' loss: %.3f' % (loss.numpy()))
 
         if split == TEST:
@@ -116,6 +125,7 @@ class Trainer(object):
                     self.best_model = copy.deepcopy(self.model)
 
         self.summary.add_scalar(split + '/total_loss_epoch', total_loss, epoch)
+        self.summary.add_scalar(split + '/sssim', total_ssim , epoch)
         print('\n=>Epoches %i, learning rate = %.6f, \previous best = %.4f' % (epoch, self.optimizer._lr_t.numpy(), self.best_loss))
 
     def inference(self):
@@ -123,13 +133,13 @@ class Trainer(object):
         Performs inference
         """
 
-        loader = self.inference_loader
+        loader = self.test_loader
         bar = tqdm(loader, total=loader.length)
         times = []
 
         for i, sample in enumerate(bar):
             image = sample[0]
-            target = None #sample[1] if len(sample) > 1 else None
+            target = sample[1] if len(sample) > 1 else None
 
             start = time.time()
             output = self.model(image)

util/general_functions.py
@@ -106,7 +106,7 @@ def get_lr_scheduler(args, global_step, loader_length):
 
     """
 
-    num_steps = int(args.epochs * loader_length / args.batch_size)
+    num_steps = int(args.epochs * loader_length)
 
     if args.lr_policy == LR_POLY_POLICY:
         lr_scheduler = tf.train.polynomial_decay(args.lr, global_step, num_steps, args.lr / 10)
@@ -310,7 +310,8 @@ def print_training_info(args):
     """
 
     print("Built ", args.model)
-    print('Dataset', args.dataset)
+    print("Dataset dir ", args.dataset_dir)
+    print('Dataset ', args.dataset)
     print('Refine network', args.refine_network)
     print('Graph execution', args.execute_graph)
 

util/summary.py
@@ -42,7 +42,7 @@ class TensorboardSummary(object):
         checkname = 'debug' if args.debug else ''
         checkname += args.model
         checkname += '_sc' if args.separable_conv else ''
-        checkname += '-refined' if args.refine_network else ''
+        checkname += '-r1' if args.refine_network else ''
         checkname += '-graph' if args.execute_graph else ''
 
         if 'deeplab' in args.model:
@@ -55,7 +55,6 @@ class TensorboardSummary(object):
             checkname += '-downs_' + str(args.num_downs) + '-ngf_' + str(args.ngf) + '-type_' + str(args.down_type)
 
         checkname += '-batch_' + str(args.batch_size)
-        checkname += '-loss_' + args.loss_type
         checkname += '-sloss_' if args.second_loss else ''
 
         if args.clip > 0:
@@ -67,7 +66,7 @@ class TensorboardSummary(object):
         checkname += '-trainval' if args.trainval else ''
 
         current_dir = os.path.dirname(__file__)
-        directory = os.path.join(current_dir, args.results_root, args.results_dir, args.dataset_dir, args.dataset, args.model, checkname)
+        directory = os.path.join(current_dir, args.results_root, args.results_dir, args.dataset_dir, args.model, checkname)
 
         runs = sorted(glob.glob(os.path.join(directory, 'experiment_*')))
         run_id = int(runs[-1].split('_')[-1]) + 1 if runs else 0
@@ -113,6 +112,8 @@ class TensorboardSummary(object):
             tf.contrib.summary.image(split + '/ZZ Image', self.image_grid(images), step=step)
             tf.contrib.summary.image(split + '/Predicted label', self.image_grid(outputs), step=step)
             tf.contrib.summary.image(split + '/Groundtruth label', self.image_grid(targets), step=step)
+
+        return images, outputs,  targets
 
     def get_step(self, split):
         """